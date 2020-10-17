6531320cb7a5cd346bd442b98a0de235ba41f287
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-10-18 01:59:36 +0530

    Server Side error handling : Register Page

--- Imagi1/assets/js/errors.js
+++ Imagi1/assets/js/errors.js
@@ -15,6 +15,36 @@ loginForm.addEventListener("submit", async (e) => {
       { email, password },
       config
     );
+    alert("You have been logged in successfully");
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => {
+        alert(error.msg);
+      });
+    }
+  }
+});
+
+const registerationForm = document.getElementById("reg-form");
+
+registerationForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await axios.post(
+      "/api/users",
+      { name, email, password },
+      config
+    );
+    alert("You have been registered successfully");
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
