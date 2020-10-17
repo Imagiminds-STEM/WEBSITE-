14ae5b2fec41775beff278b2abf496d8e62909c8
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-10-17 21:33:05 +0530

    Serving file through static server

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const connectDB = require("./src/db/mongoose");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "./Imagi1")));
 
 // Connect to database
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("Api running");
+  res.sendFile(path.join(__dirname, "./Imagi1/index.html"));
+});
+
+app.get("/events", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Imagi1/events.html"));
 });
 
 // middleware
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname+"/Imagi1"));
+app.use(express.static(__dirname + "/Imagi1"));
 
 // Define routes
 //app.use("/api/users", require("./src/routers/api/users"));
 var users = require("./src/routers/api/users.js");
-app.use("/",users);
-
+app.use("/", users);
 
 const PORT = process.env.PORT || 5000;
 
